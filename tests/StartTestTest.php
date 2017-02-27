e8d18691f81e6d0d176b9c22670dbf9680f4793d
OhmGeek <ryan@ohmgeek.co.uk>
2017-02-27 22:06:33 +0000

    New Test

---
+++ tests/StartTestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 27/02/17
+ * Time: 22:01
+ */
+use P
+class StartTestTest extends PHPUnit_Framework_TestCase
+{
+    public function firstTest()
+    {
+        $this->assertEquals(true,true);
+    }
+}
