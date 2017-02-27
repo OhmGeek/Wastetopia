0a3bf9ef157fe16d5f994c88a518206d16c40279
OhmGeek <ryan@ohmgeek.co.uk>
2017-02-27 17:36:43 +0000

    added first test

---
+++ tests/StartTest.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class StartTest extends TestCase {
+	public function firstTest() : void
+	{
+		$this->assertEquals(true,true);
+	}
+}
