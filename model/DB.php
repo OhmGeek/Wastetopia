994bc099d637a6da82d383ada11c073952456a9b
OhmGeek <ryan@ohmgeek.co.uk>
2017-03-01 22:01:07 +0000

    added error logging for testing

--- model/DB.php
+++ model/DB.php
@@ -10,11 +10,15 @@ class DB {
     public static function getDB()
 	{
 		if(!isset($_ENV['DB_HOST']) || !isset($_ENV['DB_NAME']) || !isset($_ENV['DB_USER']) || !isset($_ENV['DB_PASS'])) {
-			die("Error, no database details specified. Please run the init script.");
+			error_log("Error, no database details specified. Please run the init script.");
 		}
 
-        return new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],$_ENV['DB_USER'],$_ENV['DB_PASS']);
-    }
+        $conn = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],$_ENV['DB_USER'],$_ENV['DB_PASS']);
+        if(!$conn) {
+            error_log("Couldn't connect to DB");
+        }
+        return $conn;
+	}
 
 }
 
