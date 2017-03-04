ddbe9c08541fe7f169f4eb472e5ed357ffbfb768
stephenc29 <stephen.church@durham.ac.uk>
2017-03-04 19:02:55 +0000

    Added a router for testing polling and sending

---
+++ public/public/MessageRouter.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 04/03/2017
+ * Time: 18:40
+ */
+
+include '../control/MessageController.php';
+
+$routeType = $_GET["type"];
+
+//Polling for messages
+//Return HTML for messages in conversation
+if ($routeType == "poll"){
+    $conversationID = $_GET["conversationID"];
+    $controller = new MessageController();
+    echo($controller->generateMessageDisplay($conversationID));
+}elseif($routeType == "send"){
+    $conversationID = $_GET["conversationID"];
+    $message = $_GET["message"];
+    $controller = new MessageController();
+    $controller->sendMessage($conversationID, $message);
+}
+
+
+?>
