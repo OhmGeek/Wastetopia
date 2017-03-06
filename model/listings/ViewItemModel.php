d4435cf69c4c189b7b006366ccaab047d0f3fad2
stephenc29 <stephen.church@durham.ac.uk>
2017-03-06 16:28:17 +0000

    Added barcode query

--- model/listings/ViewItemModel.php
+++ model/listings/ViewItemModel.php
@@ -28,6 +28,7 @@ class ViewItemModel
             SELECT *
             FROM `Image` 
             JOIN `ItemImage` ON `ItemImage`.`FK_Image_ImageID` = `Image`.`ImageID`
+            JOIN `Item` ON `ItemImage`.`FK_Item_ItemID` = `Item`.`ItemID`
             JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
             WHERE `Listing`.`ListingID` = :listingID;
         ");
@@ -47,9 +48,10 @@ class ViewItemModel
      */
     function getTags($listingID){
         $statement = $this->db->prepare("
-            SELECT *
+            SELECT `Tag`.`Name`, `Tag`.`Description`, `Tag`.`FK_Category_Category_ID` as CategoryID
             FROM `Tag` 
             JOIN `ItemTag` ON `ItemTag`.`FK_Tag_TagID` = `Tag`.`TagID`
+            JOIN `Item` ON `Item`.`ItemID` = `ItemTag`.`FK_Item_ItemID`
             JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
             WHERE `Listing`.`ListingID` = :listingID;
         ");
@@ -63,18 +65,18 @@ class ViewItemModel
 
 
     /**
-     * Returns all general details about an item (location, barcode, item details and listing details)
+     * Returns all general details about an item (location, item details and listing details)
      * @param $listingID
      * @return mixed
      */
     function getDetails($listingID){
         $statement = $this->db->prepare("
-            SELECT *
-            FROM `Listing`, `Location`, `Item`, `Barcode` 
-            WHERE `Barcode`.`FK_Item_ItemID` = `Item`.`ItemID`
-            AND `Listing`.`FK_Location_LocationID` = `Location`.`LocationID`
-            AND `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
-            AND `Listing`.`ListingID` = :listingID;
+            SELECT * 
+            FROM `Listing`
+            JOIN `Location` ON `Listing`.`FK_Location_LocationID` = `Location`.`LocationID`
+            JOIN `Item` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
+            JOIN `Barcode` ON `Barcode`.`FK_Item_ItemID` = `Item`.`ItemID`
+            WHERE `Listing`.`ListingID` = :listingID;
         ");
 
         $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
@@ -84,7 +86,29 @@ class ViewItemModel
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    
+    /**
+    * Returns the barcode of an item (if it exists)
+    * @param $listingID
+    * @return int (barcode)
+    */
+    function getBarcode($listingID){
+        $statement = $this->db->prepare("
+            SELECT * 
+            FROM `Barcode`
+            JOIN `Item` ON `Item`.`ItemID` = `Barcode`.`FK_Item_ItemID`
+            JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
+            WHERE `Listing`.`ListingID` = :listingID;
+        ");
 
+        $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
+
+        $statement->execute();
+
+        return $statement->fetchColumn();
+    }
+    
+    
     /**
      * Returns all details, images and tags relating to a given listing
      * @param $listingID
@@ -94,10 +118,12 @@ class ViewItemModel
         $images = $this->getImages($listingID);
         $tags = $this->getTags($listingID);
         $generalDetails = $this->getDetails($listingID);
-
+        $barcode = $this->getBarcode($listingID);
+        
         return (array("images"=>$images,
                         "tags"=>$tags,
-                        "details"=>$generalDetails));
+                        "details"=>$generalDetails,
+                        "barcode"=>$barcode));
     }
 
 
