231119f3b8648974b2048e91d8becfd43831362b
Ryan Collins <ryan@ohmgeek.co.uk>
2017-03-26 22:51:53 +0100

    Try again, properly this time

--- public/js/ConversationListPage.js
+++ public/js/ConversationListPage.js
@@ -37,7 +37,7 @@ function loadUsers(){
 
     var givingURL = window.location.protocol + "//" + window.location.host + "/" + "messages/poll-sending";
     var receivingURL = window.location.protocol + "//" + window.location.host + "/" + "messages/poll-receiving";
-    console.log("givingURL");
+    console.log(givingURL);
     givingTab.load(givingURL);
     receivingTab.load(receivingURL); 
 }
