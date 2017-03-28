7e66a43aa7d57e02401264356c04202b417d2933
stephenc29 <stephen.church@durham.ac.uk>
2017-03-28 13:16:57 +0100

    Updated template for functions

--- controller/ProfilePageController.php
+++ controller/ProfilePageController.php
@@ -24,6 +24,11 @@ class ProfilePageController
 
     function generatePage()
     {
+        
+    }
+    
+    /* Generates the profile information HTML for the website */
+    function generateProfileSection(){
         //Get user details
         $userDetails = $this->model->getUserDetails();
 
@@ -33,10 +38,84 @@ class ProfilePageController
         $user["email"] = $userDetails["Email_Address"];
         //$user["popularityRating"] = $userDetails["Mean_Rating_Percent"];
 
-
         //Get ProfilePage twig file and display it
         $template = $this->twig->loadTemplate("TWIG_FILE");
         print($template->render(array("user"=>$user)));
+    }
+    
+    
+    /* Generates the HTML for the Listings section of the page */
+    function generateListingsSection(){
+        //Get listings user has put up
+        $userListingsSending = $this->model->getUserListings();
+        $noInterestSending = array(); //No transactions (list of ListingIDs)
+        $pendingSending = array();    //Incomplete transactions (List of TransactionIDs)
+        $completedSending = array();  //Complete transactions (List of TransactionIDs
+        
+        //Split listings into complete and pending transactions (and listings with no transactions)
+        foreach ($userListingsSending as $listing){
+            $listingID = $listing["ListingID"];
+            $stateDetails = $this->model->getStateOfListingTransaction($listingID); //Get details about the transactions
+            //If no transactions, this listing will not be in the history page
+            if (count($stateDetails) > 0){
+                foreach ($stateDetails as $transaction){
+                    $transactionID = $transaction["TransactionID"];
+                    $completed = $transaction["Sucess"];
+                    if ($completed){
+                        array_push($completedSending, $transactionID); //Get display information later
+                    }else{
+                        array_push($pendingSending, $transactionID);   //Get display information later
+                    }
+                }
+            }else{
+                array_push($noInterestSending, $listingID);            //Get display info later
+            }
+        }
+        //Get listings user is receiving
+        $userListingsReceiving = $this->model->getUserReceivingListings();
+        $pendingReceiving = array();    //Incomplete transactions
+        $completedReceiving = array();  //Incomplete transactions
+        
+        //Split listings into complete and pending transactions
+        foreach ($userListingsSending as $listing){
+            $listingID = $listing["ListingID"];
+            $stateDetails = $this->model->getStateOfListingTransaction($listingID); //Get details about the transactions
+            foreach ($stateDetails as $transaction){
+                $transactionID = $transaction["TransactionID"];
+                $completed = $transaction["Sucess"];
+                if ($completed){
+                    array_push($completedReceiving, $transactionID); //Get display information later
+                }else{
+                    array_push($pendingReceiving, $transactionID);   //Get display information later
+                }
+            }
+        }
+        
+        //Loop through arrays, getting display information (from SearchController???)
+        //Add these details to arrays
+        //Render arrays in output
+        //Get HistoryPage twig file and display it
+        $template = $this->twig->loadTemplate("TWIG_FILE");
+        print($template->render());
+    }
+    
+    
+    /* Generates the HTML for the watch list section */
+    function generateWatchListSection(){
+        //Get IDs of listings user is watching
+        $watchedListings = $this->model->getWatchedListings();
+        $watchList = array();
+        foreach($watchedListings as $listing){
+            $watchItem = array();
+            $watchItem["WatchID"] = $listing["WatchID"]; //WatchID to keep track of it
+            $listingID = $listing["ListingID"];
+            //Use SearchModel?? to get details from listingID
+            //Add these details to $watchItem
+            array_push($watchList, $watchItem);
+        }
+        //Get ProfilePage twig file and display it
+        $template = $this->twig->loadTemplate("TWIG_FILE");
+        print($template->render(array("watchList"=>$watchList)));
     }
 
 
