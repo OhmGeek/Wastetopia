be9a2407b1e6a6a126fb1b7bd13a213125e0c38f
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-01 12:22:23 +0300

    test1000

--- public/js/MessagePage.js
+++ public/js/MessagePage.js
@@ -62,7 +62,7 @@ $(function () {
 
     //Replace its inner HTML with new messages
     var url = window.location.protocol + '//' + window.location.host + '/messages/poll-messages/' + conversationID;
-    messageDisplay.load(url, function(){
+    messageDisplay.load(url.innerHTML, function(){
       $('#message-location').scrollTop($('#message-location')[0].scrollHeight);
     });
   }
