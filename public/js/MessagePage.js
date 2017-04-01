f5437d5979c32057ffe708a31979db257406429d
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-01 12:44:02 +0300

    try another method previous methods weren’t working as i thought :p

--- public/js/MessagePage.js
+++ public/js/MessagePage.js
@@ -62,7 +62,7 @@ $(function () {
 
     //Replace its inner HTML with new messages
     var url = window.location.protocol + '//' + window.location.host + '/messages/poll-messages/' + conversationID;
-    messageDisplay.load(url.innerHTML, function(){
+    messageDisplay.load(url + ' #message-location', function(){
       console.log("i was told to scroll to the bottom");
       scrollToBottom();
     });
