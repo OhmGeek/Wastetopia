80aa3681a2aeb4b9bc5cfca60c2415f10e13785d
stephenc29 <stephen.church@durham.ac.uk>
2017-04-03 15:41:52 +0100

    Added most frequent tags for received items

--- model/AnalysisModel.php
+++ model/AnalysisModel.php
@@ -29,15 +29,41 @@ class AnalysisModel
     {
         //$reader = new UserCookieReader();
         //return $reader->get_user_id();
-        return 20; //Hardcoded for now
+        return 6; //Hardcoded for now
     }
 
-
+   //BOTH QUERIES DONE USING CATEGORYID = 1 (May change later)
     /**
-     * Gets a list of Tag Names along with their frequencies
+     * Gets a list of Tag Names along with their frequencies for current user's listings
      * @return array
      */
-    function getTagFrequencies()
+    function getTagFrequenciesForListings()
+    {
+        $userID = $this->getUserID();
+
+        $statement = $this->db->prepare("
+        SELECT `Tag`.`Name`, COUNT(*) as `Count`
+                FROM `Tag` 
+                JOIN `ItemTag` ON `ItemTag`. `FK_Tag_TagID` = `Tag`.`TagID`
+                JOIN `Item` ON `Item`.`ItemID` = `ItemTag`.`FK_Item_ItemID`
+                JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
+                JOIN `User` ON `UserID` = `Listing`.`FK_User_UserID`
+                WHERE `User`.`UserID` = :userID
+                AND `Tag`.`FK_Category_Category_ID` = 1
+                GROUP BY `Tag`.`Name`
+                ORDER BY `Count`;");
+
+        $statement->bindValue(":userID", $userID, PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+    
+    /**
+     * Gets a list of Tag Names along with their frequencies for items the user has received 
+     * @return array
+     */
+    function getTagFrequenciesForTransactions()
     {
         $userID = $this->getUserID();
 
@@ -47,14 +73,18 @@ class AnalysisModel
                 JOIN `ItemTag` ON `ItemTag`. `FK_Tag_TagID` = `Tag`.`TagID`
                 JOIN `Item` ON `Item`.`ItemID` = `ItemTag`.`FK_Item_ItemID`
                 JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
-                JOIN `User` ON `UserID` = `Listing`.`FK_User_UserID`
-                WHERE `User`.`UserID` = 2
+                JOIN `ListingTransaction` ON `ListingTransaction`.`FK_Listing_ListingID` = `Listing`.`ListingID`
+                JOIN `Transaction` ON `Transaction`.`TransactionID` = `ListingTransaction`.`FK_Transaction_TransactionID`
+                JOIN `User` ON `UserID` = `Transaction`.`FK_User_UserID`
+                WHERE `User`.`UserID` = :userID
                 AND `Tag`.`FK_Category_Category_ID` = 1
-                GROUP BY `Tag`.`Name`;");
+                AND `ListingTransaction`.`Success` = 1
+                GROUP BY `Tag`.`Name`
+                ORDER BY `Count`;");
 
         $statement->bindValue(":userID", $userID, PDO::PARAM_STR);
         $statement->execute();
 
         return $statement->fetchAll(PDO::FETCH_ASSOC);
-}
+    }
 }
