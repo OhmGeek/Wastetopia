115bd42fcc4f3896fdac57ca1034ac0f31f298b7
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-04 12:20:43 +0300

    i hate google maps a lot part2

--- view/html_version/map.js
+++ view/html_version/map.js
@@ -2,18 +2,7 @@
 // When the user clicks the marker, an info window opens.
 
 function initMap() {
-  //custom marker
-  //adapted from http://gmaps-samples-v3.googlecode.com/svn/trunk/overlayview/custommarker.html
-  function CustomMarker(latlng, map, imageSrc) {
-    this.latlng_ = latlng;
-    this.imageSrc = imageSrc;
-    // Once the LatLng and text are set, add the overlay to the map.  This will
-    // trigger a call to panes_changed which should in turn call draw.
-    this.setMap(map);
-  }
-
-  var uluru = {lat: 37.77088429547992, lng: -122.4135623872337};
-
+  var uluru = {lat: -25.363, lng: 131.044};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: uluru,
@@ -47,49 +36,37 @@ function initMap() {
     content: contentString, maxwidth: 300
   });
 
-  CustomMarker.prototype = new google.maps.OverlayView();
-
-  CustomMarker.prototype.draw = function () {
-    // Check if the div has been created.
-    var div = this.div_;
-    if (!div) {
-      // Create a overlay text DIV
-      div = this.div_ = document.createElement('div');
-      // Create the DIV representing our CustomMarker
-      div.className = "customMarker"
-
-      var img = document.createElement("img");
-      img.src = this.imageSrc;
-      div.appendChild(img);
-
-      google.maps.event.addDomListener(div, "click", function (event) {
-            console.log(event.latLng)
-      });
-
-      // Then add the overlay to the DOM
-      var panes = this.getPanes();
-      panes.overlayImage.appendChild(div);
-    }
-
-    // Position the overlay
-    var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
-    if (point) {
-      div.style.left = point.x + 'px';
-      div.style.top = point.y + 'px';
-    }
+  var markerIcon = {
+    url: 'BUS.jpg',
+    //The size image file.
+    scaledSize: new google.maps.Size(225, 120),
+    //The point on the image to measure the anchor from. 0, 0 is the top left.
+    origin: new google.maps.Point(0, 0),
+    //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
+    anchor: new google.maps.Point(189, 116)
   };
 
-  CustomMarker.prototype.remove = function () {
-    // Check if the overlay was on the map and needs to be removed.
-    if (this.div_) {
-      this.div_.parentNode.removeChild(this.div_);
-      this.div_ = null;
-    }
+  //Setting the shape to be used with the Glastonbury map marker.
+  var markerShape = {
+        coord: [12,4,216,22,212,74,157,70,184,111,125,67,6,56],
+        type: 'poly'
   };
 
-  CustomMarker.prototype.getPosition = function () {
-    return this.latlng_;
-  };
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: markerIcon,
+    shape: markerShape
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  // Event that closes the Info Window with a click on the map
+  map.addListener('click', function() {
+    infowindow.close();
+  });
 
   /* this part of the code is from http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html */
   /*
@@ -127,28 +104,4 @@ function initMap() {
     var iwCloseImg = iwCloseBtn.children(':nth-child(1)').attr('src','icons/close.png');
     iwCloseImg.css({width: '100%', height : '100%', position: 'relative', top:'0',left:'0'})
   });
-
-  // Event that closes the Info Window with a click on the map
-  map.addListener('click', function() {
-    infowindow.close();
-  });
-
-  var data = [{
-    profileImage: "http://www.gravatar.com/avatar/d735414fa8687e8874783702f6c96fa6?s=90&d=identicon&r=PG",
-    pos: [37.77085, -122.41356]
-  }, {
-    profileImage: "http://placekitten.com/90/90",
-    pos: [37.77220, -122.41555]
-  }, {
-    profileImage: "flowery.jpg",
-    pos: uluru
-  }]
-
-  for(var i=0;i<data.length;i++){
-    var marker = new CustomMarker(new google.maps.LatLng(data[i].pos[0],data[i].pos[1]), map,  data[i].profileImage)
-    marker.addListener('click', function(event){
-      console.log('click')
-      infowindow.open(map,marker)
-    })
-  }
 };
