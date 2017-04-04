0af56e3939575794a726984f4f1645677f86fbdd
stephenc29 <stephen.church@durham.ac.uk>
2017-04-04 11:50:19 +0100

    Removed functions for cardDetails and userDetails

--- model/ProfilePageModel.php
+++ model/ProfilePageModel.php
@@ -28,65 +28,7 @@ class ProfilePageModel
     {
         return $this->userID;
     }
-    /**
-     * Returns the details needed for display on the profile page given the listing ID
-     *
-     * @param $listingID
-     * @return mixed
-     */
-    function getCardDetails($listingID){
-        $statement = $this->db->prepare("
-        SELECT `Listing`.`ListingID`, `Listing`.`Quantity`, `Listing`.`Time_Of_Creation`,
-                `Item`.`ItemID`, `Item`.`Name`, `Item`.`Description`, 
-                `Location`.`Post_Code`,
-                `User`.`UserID`, `User`.`Forename`, `User`.`Surname`
-        FROM `Listing`
-        JOIN `User` ON `Listing`.`FK_User_UserID` = `User`.`UserID`
-        JOIN `Location` ON `Listing`.`FK_Location_LocationID` = `Location`.`LocationID`
-        JOIN `Item` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
-        WHERE `Listing`.`ListingID` = :listingID;
-        ");
-        $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
-        $statement->execute();
-        $results =  $statement->fetchAll(PDO::FETCH_ASSOC);
-        return $results[0];
-    }
-    /**
-     * Gets all detail from the User table for this user
-     * @return mixed
-     */
-    function getUserDetails(){
-        $userID = $this->getUserID();
-        $statement = $this->db->prepare("
-        SELECT * 
-        FROM `User` 
-        WHERE `UserID` = :userID
-        ");
-        $statement->bindValue(":userID", $userID, PDO::PARAM_INT);
-        $statement->execute();
-        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        return $results[0];
-    }
-
-    /**
-     * Gets the profile picture of the given user (Possibly will be moved to another model)
-     * @param $userID
-     * @return URL
-     */
-    function getUserImage($userID)
-    {
-        $statement = $this->db->prepare("
-                                SELECT Picture_URL
-                                FROM `User`
-                                WHERE `User`.`UserID` = :userID
-							");
-
-        $statement->bindValue(':userID', $userID, PDO::PARAM_INT);
-
-        $statement->execute();
-
-        return $statement->fetchColumn();
-    }
+    
 
 
     /**
@@ -383,24 +325,4 @@ class ProfilePageModel
         return;
     }
 
-    /**
-     * Returns the default image for this listing (if there is one)
-     * @param $listingID
-     * @return mixed
-     */
-    function getDefaultImage($listingID){
-        $statement = $this->db->prepare("
-            SELECT `Image`.`Image_URL` 
-            FROM `Image`
-            JOIN `ItemImage` ON `ItemImage`.`FK_Image_ImageID` = `Image`.`ImageID`
-            JOIN `Item` ON `ItemImage`.`FK_Item_ItemID` = `Item`.`ItemID`
-            JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
-            WHERE `Listing`.`ListingID` = :listingID
-            AND `ItemImage`.`Is_Default` = 1;
-        ");
-        $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
-        $statement->execute();
-        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        return $results[0];
-    }
 }
