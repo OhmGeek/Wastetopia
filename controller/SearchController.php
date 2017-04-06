43a4eb869eeda5726247b1cc25e74ddc3b2098ca
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2017-04-06 15:04:06 +0100

    Remove unessesary fucntions

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -107,51 +107,6 @@ class SearchController
         return $searchResults;
     }
 
-    public function sampleSearch()
-    {
-        $var = '[{
-                    "lat": 54.767289,
-                    "long": -1.570361,
-                    "img": "flowers.jpg",
-                    "username": "Chen Hemsworth",
-                    "user_id": 101,
-                    "date_added": "29/03/17",
-                    "item_name": "Pina Collada",
-                    "item_id": 301
-                  },
-                  {
-                    "lat": 54.767672,
-                    "long": -1.570551,
-                    "img": "fruit.jpg",
-                    "username": "Bryan Collins",
-                    "user_id": 102,
-                    "date_added": "29/03/17",
-                    "item_name": "Strawberry Daquri",
-                    "item_id": 302
-                  },
-                  {
-                    "lat": 54.767441,
-                    "long": -1.57204,
-                    "img": "veg.jpg",
-                    "username": "Stephan Church",
-                    "user_id": 103,
-                    "date_added": "29/03/17",
-                    "item_name": "Mojito",
-                    "item_id": 303
-                  },
-                  {
-                    "lat": 54.767441,
-                    "long": -1.57204,
-                    "img": "donut.jpg",
-                    "username": "Stephan Church",
-                    "user_id": 103,
-                    "date_added": "29/03/17",
-                    "item_name": "Margharita",
-                    "item_id": 304
-                  }]';
-        return $var;
-    }
-
     /*Calculate the distance between point 1 and 2 using the Haversine formula*/
     function haversineDistance($latLong1, $latLong2)
     {
