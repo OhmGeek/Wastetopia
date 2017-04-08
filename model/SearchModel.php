fa7f6f3d0347f55dd1e795bd97a5ee37e28e857f
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2017-04-08 16:17:51 +0100

    Revert "Remove unessesary fucntions"
    This reverts commit 43a4eb869eeda5726247b1cc25e74ddc3b2098ca.

--- model/SearchModel.php
+++ model/SearchModel.php
@@ -24,6 +24,66 @@ class SearchModel
     {
         $this->db = DB::getDB();
     }
+    //DISPLAY FUNCTIONS HERE: RETURNS DETAILS NEEDED FOR DISPLAY GIVEN THE LISTING IDs
+    /**
+     * Returns the details needed for display on the search page given the listing ID
+     *
+     * @param $listingID
+     * @return mixed
+     */
+    function getCardDetails($listingID){
+        $statement = $this->db->prepare("
+            SELECT `Listing`.`ListingID`, `Listing`.`Quantity`, `Listing`.`Time_Of_Creation`,
+                `Item`.`ItemID`, `Item`.`Name`, `Item`.`Description`, 
+                `Location`.`Post_Code`,
+                `User`.`UserID`, `User`.`Forename`, `User`.`Surname`
+        FROM `Listing`
+        JOIN `User` ON `Listing`.`FK_User_UserID` = `User`.`UserID`
+        JOIN `Location` ON `Listing`.`FK_Location_LocationID` = `Location`.`LocationID`
+        JOIN `Item` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
+        WHERE `Listing`.`ListingID` = :listingID;
+        ");
+        $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    /**
+     * Returns the default image for this listing (if there is one)
+     * @param $listingID
+     * @return mixed
+     */
+    function getItemImage($listingID){
+        $statement = $this->db->prepare("
+            SELECT `Image`.`Image_URL` 
+            FROM `Image`
+            JOIN `ItemImage` ON `ItemImage`.`FK_Image_ImageID` = `Image`.`ImageID`
+            JOIN `Item` ON `ItemImage`.`FK_Item_ItemID` = `Item`.`ItemID`
+            JOIN `Listing` ON `Listing`.`FK_Item_ItemID` = `Item`.`ItemID`
+            WHERE `Listing`.`ListingID` = :listingID
+            AND `ItemImage`.`Is_Default` = 1;
+        ");
+        $statement->bindValue(":listingID", $listingID, PDO::PARAM_INT);
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $results[0]; 
+    }
+    /**
+     * Gets the profile picture of the given user (Possibly will be moved to another model)
+     * @param $userID
+     * @return URL
+     */
+    function getUserImage($userID)
+    {
+        $statement = $this->db->prepare("
+                                SELECT Picture_URL
+                                FROM `User`
+                                WHERE `User`.`UserID` = :userID
+                            ");
+        $statement->bindValue(':userID', $userID, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchColumn();
+    }
 
     /*Distance searches will return listings only within 0.76 degrees of search location
       This draws a square of hight 77km and of variable width based on the latitude
