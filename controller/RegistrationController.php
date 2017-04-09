d5edb3371c3837347e4bafd2c8cffa3c6e8c8ded
stephenc29 <stephen.church@durham.ac.uk>
2017-04-09 15:44:51 +0100

    Updated to return JSON

---
+++ controller/RegistrationController.php
@@ -0,0 +1,100 @@
+<?php
+namespace Wastetopia\Controller;
+
+use Wastetopia\Model\RegistrationModel;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
+use Wastetopia\Config\CurrentConfig;
+
+class RegistrationController
+{
+
+    public function __construct()
+    {
+        $this->model = new RegistrationModel();
+
+        $loader = new Twig_Loader_Filesystem('../view/');
+        $this->twig = new Twig_Environment($loader);
+    }
+
+    /**
+     * Generates the HTML for the Registration page
+     * @return mixed
+     */
+    function generatePage()
+    {
+        $template = $this->twig->loadTemplate('users/registration.twig'); // Need to add Twig file
+
+        $CurrentConfig = new CurrentConfig();
+        $config = $CurrentConfig->getAll();
+
+        return $template->render(array("config" => $config)); // Need to pass config stuff
+    }
+
+    /**
+     * Checks if the given email already exists in the database
+     * @param $email
+     * @return mixed
+     */
+    function checkAvailable($email){
+        $temp = $this->model->checkEmailExists($email);
+        // $temp will be True if the email exists, want to then return False
+        return !$temp;
+    }
+
+    /**
+     * Checks the two given passwords match
+     * @param $pwd
+     * @param $pwdConfirm
+     * @return bool
+     */
+    function checkPassword($pwd, $pwdConfirm){
+        if($pwd == $pwdConfirm){
+            return True;
+        }
+        return False;
+    }
+
+    /**
+     * Main function to add user (performs all the checks)
+     * @param $forename
+     * @param $surname
+     * @param $email
+     * @param $password
+     * @param $passwordConfirm
+     * @param null $pictureURL (If not given, will save default image)
+     * @return JSON (in form {"error":...} or {"success":...}
+     */
+    function addUser($forename, $surname, $email, $password, $passwordConfirm, $pictureURL = NULL){
+        // Check passwords match
+        if(!(checkPassword($password, $passwordConfirm))){
+            return $this->errorMessage("Passwords don't match");
+        }
+        // Check the email is available(i.e, not already in use)
+        elseif(!checkAvailable($email)){
+            return $this->errorMessage("Email already in use");
+        }
+        else {
+            // Add user to DB
+            $result = $this->model->addUser($forename, $surname, $email, $password, $pictureURL);
+            // Send back success or error message
+            if (!($result)) {
+                return $this->errorMessage("Couldn't add user (something unexpected went wrong)");
+            } else {
+                return $this->successMessage("Success");
+            }
+        }
+    }
+
+    function errorMessage($e){
+        $errorArray = array("error" => $e);
+        return json_encode($errorArray);
+    }
+
+    function successMessage($s){
+        $successArray = array("success" => $s);
+        return json_encode($successArray);
+    }
+}
+?>
