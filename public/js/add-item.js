4603747dfe5c4d752c4c8c4ceceeb00724c385af
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-10 13:46:56 +0300

    still not done need to add the missing js and css for add map

--- public/js/add-item.js
+++ public/js/add-item.js
@@ -1,106 +1,154 @@
-function showUploadedItem(url, id) {
-    var item = $('div').html = '<div>' +
-                                    '<div class="col-xs-4 col-sm-2 zero-padding">'+
-                                        '<div class="row-action-primary checkbox img-checkbox">'+
-                                            '<label><input type="checkbox"></label>'+
-                                        '</div>'+
-                                    '<div data-mh="my-group" class="upload-pic">'+
-                                        '<img src="' + url + '" data-imgid="' + id + '"</div>'+
-                                    '</div>'+
-                                '</div>';
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer'
+  }
+});
 
-    //now we add the item div to the page.
-    $('#image-demo').before(item);
+// Remove an element from the layout in the grid
+function remove(elem) {
+  // remove clicked element (in a very skitchy way right now)
+  $grid.isotope( 'remove', $(elem).closest('.grid-item'))
+  // layout remaining item elements
+  .isotope('layout');
+};
+
+// Remove selected images
+$('#deleteBtn').on( 'click', function() {
+  var $checkboxes = $('.item-imgs-section input');
+  // inclusive filters from checkboxes
+  $checkboxes.each( function( i, elem ) {
+    // if checkbox, use value if checked
+    if ( elem.checked ) {
+      remove(elem)
+    }
+  });
+});
+
+function showUploadedItem(url, id) {
+  var $item = $('<div class="grid-item col-xs-4 col-sm-2 zero-padding">'+
+  '<div class="row-action-primary checkbox img-checkbox">'+
+  '<label><input type="checkbox"></label>'+
+  '</div>'+
+  '<div data-mh="my-group" class="upload-pic">'+
+  '<img src="'+ url +'" data-imgid="' + id + '"</div>'+
+  '</div>'+
+  '</div>');
+
+  // prepend items to grid
+  $grid.prepend( $item )
+  // add and lay out newly prepended items
+  .isotope( 'prepended', $item );
 }
 
-
 function imageUpload() {
-    // go through and get the images
-    var formdata = new FormData($('#form-image')[0]);
-    formdata.append('image', $('input[type=file]')[0].files[0]); // todo add all files
-    $.ajax({
-        url: 'https://wastetopia-pr-17.herokuapp.com/api/items/addimage',
-        type: "POST",
-        data: formdata,
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function (res) {
-            var items = JSON.parse(res);
-            items.forEach(function(item) {
-                showUploadedItem(item.url, item.id);
-            });
-        }
-    });
+  // go through and get the images
+  var formdata = new FormData($('#form-image')[0]);
+  formdata.append('image', $('input[type=file]')[0].files[0]); // todo add all files
+  $.ajax({
+    url: 'https://wastetopia-pr-17.herokuapp.com/api/items/addimage',
+    type: "POST",
+    data: formdata,
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (res) {
+      var items = JSON.parse(res);
+      items.forEach(function(item) {
+        showUploadedItem(item.url, item.id);
+      });
+    }
+  });
 
 }
 
 $('#form-image').change(function() {
-    imageUpload();
+  imageUpload();
 });
 
 function getImagesFromDOM() {
-    var imageList = [];
-    $('.upload-pic img').forEach(function(elem) {
-        imageList.append(elem.href); //todo check this - we want to get the href of the image tag
-    });
-    return imageList;
+  var imageList = [];
+  $('.upload-pic img').forEach(function(elem) {
+    imageList.append(elem.href); //todo check this - we want to get the href of the image tag
+  });
+  return imageList;
 }
 
 function getSelectedDietaryReqs() {
-    var requirementsList = [];
-    // todo use filter to get this working nicely.
-    $('.dietary-req').forEach(function(elem) {
-        // if the selected checkbox is actually selected, add the item
-        // to the requirements list. Otherwise, move to the next one.
-        if(elem.val()) {
-            requirementsList.append(elem.text); //get the text (or the contents of the tag).
-        }
-    });
-
-    // check if the user has any other requirements that need fulfilling.
-    var otherRequirements = $('#other-req').val();
-
-    if(otherRequirements) {
-        requirementsList.append(otherRequirements);
+  var requirementsList = [];
+  // todo use filter to get this working nicely.
+  $('.dietary-req').forEach(function(elem) {
+    // if the selected checkbox is actually selected, add the item
+    // to the requirements list. Otherwise, move to the next one.
+    if(elem.val()) {
+      requirementsList.append(elem.text); //get the text (or the contents of the tag).
     }
+  });
 
-    return requirementsList;
+  // check if the user has any other requirements that need fulfilling.
+  var otherRequirements = $('#other-req').val();
+
+  if(otherRequirements) {
+    requirementsList.append(otherRequirements);
+  }
+
+  return requirementsList;
 }
 
 function getMayContainsDetails() {
-    var mayContain = [];
-    $('.may-contains-select option').forEach(function(elem) {
-        if(elem.val()) {
-            mayContain.append(elem.text);
-        }
-    });
-    return mayContain;
+  var mayContain = [];
+  $('.may-contains-select option').forEach(function(elem) {
+    if(elem.val()) {
+      mayContain.append(elem.text);
+    }
+  });
+  return mayContain;
 }
 
 function getLocationOfItem() {
-    // todo use Google Maps/Ben's API
-    var location = {
-        "state": Alabama,
-        "firstLineAddr": 23 Frances,
-        "secondLineAddr" Postcode
+  // todo use Google Maps/Ben's API
+  // var location = {
+  //     "state": Alabama,
+  //     "firstLineAddr": 23 Frances,
+  //     "secondLineAddr": Postcode
+  // }
+  //must include google api link
+  //can just use the postcode and country then make a request to google api to get the longitude, latitude
+  //(need this for map search :p) and city < can make this as one of the option no need to use the api
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    componentRestrictions: {
+      country: $('#counrty').val(),
+      postalCode: $('#postcode').val()
     }
+  },
+
+  function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      return results[0].geometry.location,
+    } else {
+      alert("geocode of " + $('#postcode').val() + " failed:" + status);
+    }
+  });
 }
+
 function serializeItem() {
-    //todo: process expiry date (need more research into this). Think it's just .val, but not fully sure.
-    // todo: process item type properly.
-    // todo: check errors in images/date/location
+  //todo: process expiry date (need more research into this). Think it's just .val, but not fully sure.
+  // todo: process item type properly.
+  // todo: check errors in images/date/location
 
-    var item = {
-        "name": $('#name').val(),
-        "images": getImagesFromDOM(),
-        "classification": itemType,
-        "dietary": getSelectedDietaryReqs(),
-        "contains": getMayContainsDetails(),
-        "expires": expiryDate,
-        "description": $('description').val(),
-        "location": getLocationOfItem()
-    };
+  var item = {
+    "name": $('#name').val(),
+    "images": getImagesFromDOM(),
+    "classification": itemType,
+    "dietary": getSelectedDietaryReqs(),
+    "contains": getMayContainsDetails(),
+    "expires": expiryDate,
+    "description": $('description').val(),
+    "location": getLocationOfItem()
+  };
 
-    return item;
+  return item;
 }
