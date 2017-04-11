4d1372aad65e20381326a76d197c31907f009549
stephenc29 <stephen.church@durham.ac.uk>
2017-04-12 00:07:01 +0100

    Update RequestModel.php

--- model/RequestModel.php
+++ model/RequestModel.php
@@ -27,7 +27,7 @@ class RequestModel
     {
 //        $reader = new UserCookieReader();
 //        return $reader->get_user_id();
-        return 1; //Hardcoded for now
+        return 6; //Hardcoded for now
     }
 
 	
