eb0373bb8391f1f0f93a375b355f321ec037c634
stephenc29 <stephen.church@durham.ac.uk>
2017-04-11 14:47:34 +0100

    Changed to use live instead of on

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,7 +8,7 @@ $(function () {
     }
   });
 
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  $('a[data-toggle="tab"]').live('shown.bs.tab', function (e) {
     console.log($(e.target).attr("href"));
     var width = 150;
     var height = 150;
@@ -32,7 +32,7 @@ $(function () {
   });
 
   // Reload HTML content when Back buttons pressed on each tab - not working
-  $('a[href="#home"]').click(function (){
+  $('a[href="#home"]').live('click',function (){
     console.log("Reloading");
     var userID = $('.user-name').attr("id");
     var url = window.location.protocol + "//" + window.location.host + "/profile/update/" + userID;
@@ -43,7 +43,7 @@ $(function () {
    });
 
   // Delete a card from the page
-  $grid.on( 'click', '#delete', function() {
+  $grid.live('click', '#delete', function() {
     // get the id of the item would be removed
     console.log($(this).closest('.thumbnail').attr("id"));
     remove(this)
