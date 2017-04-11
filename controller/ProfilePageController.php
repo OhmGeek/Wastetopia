0aef51046d24f7307983c3068885595549763e5b
stephenc29 <stephen.church@durham.ac.uk>
2017-04-11 13:44:32 +0100

    Update ProfilePageController.php

--- controller/ProfilePageController.php
+++ controller/ProfilePageController.php
@@ -50,7 +50,7 @@ class ProfilePageController
     {
         // $reader = new UserCookieReader();
         // return $reader->get_user_id();
-        return 20; // Usually 6
+        return 6; // Usually 6
     }
 
     function generatePage(){
