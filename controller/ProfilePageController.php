f1b40dc433928196d5fb271078ef000ea929cb09
stephenc29 <stephen.church@durham.ac.uk>
2017-04-11 19:02:46 +0100

    Added setAllPendingAsViewed

--- controller/ProfilePageController.php
+++ controller/ProfilePageController.php
@@ -13,6 +13,9 @@ use Wastetopia\Model\CardDetailsModel;
 use Wastetopia\Controller\RecommendationController;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
+
+use Wastetopia\Model\RequestModel;
+
 class ProfilePageController
 {
     /**
@@ -30,6 +33,9 @@ class ProfilePageController
 
         // Card details model
         $this->cardDetailsModel = new CardDetailsModel();
+        
+        // Request model
+        $this->requestModel = new RequestModel();
 
         // Profile page model
         $this->model = new ProfilePageModel($this->userID); //Need to include
@@ -107,285 +113,6 @@ class ProfilePageController
         $userInformation["userimage"] = $userImage;
         return $userInformation;
     }
-
-//    /* Generates the information for the Listings section of the page
-//    TODO: Sort out difference between a transaction and a listing
-//    TODO: Show users transactions for pending and completed with the quantities from ListingTransaction table */
-//    function generateTabsSection()
-//    {
-//        //Get listings user has put up
-//        $userListingsSending = $this->model->getUserListings();
-//
-//        $pendingSending = array();    //Incomplete transactions (List of TransactionIDs)
-//        $completedSending = array();  //Complete transactions (List of TransactionIDs
-//        $availableListingsSending = array(); // List of all listingIDs user is giving away that have quantity
-//        $emptyListingsSending = array(); // List of all listingIDs user is giving away that have 0 quantity
-//        // Total number of listings the user has offered
-//        // Should this be changed to not include completed listings?? (i.e 0 quantity)
-//        $listingsCount = count($userListingsSending);
-//        // total number of transactions that have been made for user's listings
-//        $sendingTransactionsCount = 0;
-//        //Split listings into complete and pending transactions (and listings with no transactions)
-//        foreach ($userListingsSending as $listing) {
-//            $listingID = $listing["ListingID"];
-//            $listingQuantity = $listing["Quantity"]; // Will be 0 if listing has run out
-//            $active = $listing["Active"]; // Will be 0 if user no longer wants to see it
-//            // Only process active listings
-//
-//            //Get details about the transactions involving this listing
-//            $stateDetails = $this->model->getStateOfListingTransaction($listingID);
-//            //If no transactions, this listing will not be in the history page
-//            if (count($stateDetails) > 0) {
-//                foreach ($stateDetails as $transaction) {
-//
-//                    $transactionID = $transaction["TransactionID"];
-//                    $completed = $transaction["Success"];
-//                    if ($completed == 1) {
-//                        $sendingTransactionsCount += 1;
-//                        // Need to figure out how to deal with these as transactions
-//                        array_push($completedSending, $transactionID); //Get display information later
-//                    }elseif($completed == 0) {
-//                        $sendingTransactionsCount += 1;
-//                        // Need to figure out how to deal with these as transactions
-//                        array_push($pendingSending, $transactionID);   //Get display information later
-//                    }else{
-//                        //Do nothing	- it transaction has been rejected
-//                    }
-//                }
-//            }
-//            if($active){
-//                // Check whether it has quantity or not
-//                if ($listingQuantity > 0){
-//                    array_push($availableListingsSending, $listingID);
-//                }else{
-//                    array_push($emptyListingsSending, $listingID);
-//                }
-//            }
-//        }
-//        $totalAvailabaleListings = count($availableListingsSending);
-//        $totalEmptyListings = count($emptyListingsSending);
-//        //Get listings user is receiving
-//        $userListingsReceiving = $this->model->getUserReceivingListings();
-//        $pendingReceiving = array();    //Incomplete transactions
-//        $completedReceiving = array();  //Incomplete transactions
-//
-//        // Total number of listings the user has requested
-//        // Should this be changed to not include completed listings?
-//        $receivingCount = 0;
-//
-//        //Split listings into complete and pending transactions
-//        foreach ($userListingsReceiving as $listing) {
-//            $listingID = $listing["ListingID"];
-//            $transactionID = $listing["TransactionID"];
-//            $completed = $listing["Success"]; // Transaction completed?
-//            if ($completed == 1) {
-//                $receivingCount += 1;
-//                // Need to figure out how to deal with these as transactions
-//                array_push($completedReceiving, $transactionID); //Get display information later
-//            } elseif($completed == 0) {
-//                $receivingCount += 1;
-//                // Need to figure out how to deal with these as transactions
-//                array_push($pendingReceiving, $transactionID);   //Get display information later
-//            }else{
-//                // Do nothing - Rejected it
-//            }
-//        }
-//        // NOW GET APPROPRIATE INFORMATION FROM EACH LISTING
-//        $allAvailableListings = array();
-//        $allEmptyListings = array();
-//        $completedOffers = array();
-//        $pendingOffers = array();
-//        $completedRequests = array();
-//        $pendingRequests = array();
-//        // Get information for all user's listings
-//        foreach ($availableListingsSending as $listingID) {
-//            $details = $this->cardDetailsModel->getCardDetails($listingID);
-////            $itemID = $details["ItemID"];
-//            $itemName = $details["Name"];
-//            $timeOfCreation = $details["Time_Of_Creation"];
-//            $quantity = $details["Quantity"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//            $isRequesting = $this->model->isRequesting($listingID, $this->getUserID());
-//            $isWatching = $this->inWatchList($listingID);
-//            $item = array(
-//                "listingID" => $listingID,
-//                "itemName" => $itemName,
-//                "addedDate" => $timeOfCreation,
-//                "quantity" => $quantity,
-//                "imgURL" => $imageURL,
-//                "isRequesting" => $isRequesting,
-//                "isWatching" => $isWatching);
-//            array_push($allAvailableListings, $item);
-//        }
-//        // Get information for all user's empty listings
-//        foreach ($emptyListingsSending as $listingID) {
-//            $details = $this->cardDetailsModel->getCardDetails($listingID);
-////            $itemID = $details["ItemID"];
-//            $itemName = $details["Name"];
-//            $timeOfCreation = $details["Time_Of_Creation"];
-//            $quantity = $details["Quantity"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//
-//            // Figure out whether to display "Request" or "Cancel request" button
-//            // $isRequesting = $this->cardDetailsModel->isUserRequestingListing($this->userID, $listingID);
-//
-//            $item = array(
-//                "listingID" => $listingID,
-//                "itemName" => $itemName,
-//                "addedDate" => $timeOfCreation,
-//                "quantity" => $quantity,
-//                "imgURL" => $imageURL);
-//            array_push($allEmptyListings, $item);
-//        }
-//        //NEXT TWO FOR LOOPS ARE ALMOST IDENTICAL, CHANGE NAMES IN TWIG SO THESE CAN BE MADE INTO ONE FUNCTION
-//        // Completed sending transactions
-//        foreach ($completedSending as $transactionID) {
-//            $transactionDetails = $this->model->getDetailsFromTransactionID($transactionID);
-//            // Info specific to transaction
-//            $transactionQuantity = $transactionDetails["Quantity"];
-//            $completedDate = $transactionDetails["Time_Of_Acceptance"];
-//            //In for about user involved in this transaction
-//            $requestingUserID = $transactionDetails["UserID"];
-//            $requestingUserName = $transactionDetails["Forename"] . " " . $transactionDetails["Surname"];
-//            $requestingUserImage = $this->cardDetailsModel->getUserImage($requestingUserID);
-//            // Information about the item/listing involved
-//            $listingID = $transactionDetails["ListingID"];
-//            $listingDetails = $this->cardDetailsModel->getCardDetails($listingID);
-//            $itemName = $listingDetails["Name"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//            $item = array(
-//                "transactionID" => $transactionID,
-//                "completedDate" => $completedDate,
-//                "imgURL" => $imageURL,
-//                "itemName" => $itemName,
-//                "userImg" => $requestingUserImage,
-//                "userID" => $requestingUserID,
-//                "userName" => $requestingUserName,
-//                "quantity" => $transactionQuantity,
-//                "listingID" => $listingID
-//            );
-//            array_push($completedOffers, $item);
-//        }
-//        // Pending sending transactions
-//        foreach ($pendingSending as $transactionID) {
-//            $transactionDetails = $this->model->getDetailsFromTransactionID($transactionID);
-//            // Info specific to transaction
-//            $transactionQuantity = $transactionDetails["Quantity"];
-//            $startedDate = $transactionDetails["Time_Of_Application"];
-//            //In for about user involved in this transaction
-//            $requestingUserID = $transactionDetails["UserID"];
-//            $requestingUserName = $transactionDetails["Forename"] . " " . $transactionDetails["Surname"];
-//            $requestingUserImage = $this->cardDetailsModel->getUserImage($requestingUserID);
-//            // Information about the item/listing involved
-//            $listingID = $transactionDetails["ListingID"];
-//            $listingDetails = $this->cardDetailsModel->getCardDetails($listingID);
-//            $itemName = $listingDetails["Name"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//            $item = array(
-//                "transactionID" => $transactionID,
-//                "startedDate" => $startedDate,
-//                "imgURL" => $imageURL,
-//                "itemName" => $itemName,
-//                "userImg" => $requestingUserImage,
-//                "userID" => $requestingUserID,
-//                "userName" => $requestingUserName,
-//                "quantity" => $transactionQuantity,
-//                "listingID" => $listingID,
-//                "conversationID" => "REPLACE WITH LISTING ID" // Need to figure this out
-//            );
-//            array_push($pendingOffers, $item);
-//        }
-//
-//        // Completed requesting transactions
-//        foreach ($completedReceiving as $transactionID) {
-//            $transactionDetails = $this->model->getDetailsFromTransactionID($transactionID);
-//            // Info specific to transaction
-//            $transactionQuantity = $transactionDetails["Quantity"];
-//            $completedDate = $transactionDetails["Time_Of_Acceptance"];
-//            // Information about the item/listing involved
-//            $listingID = $transactionDetails["ListingID"];
-//            $listingDetails = $this->cardDetailsModel->getCardDetails($listingID);
-//            $itemName = $listingDetails["Name"];
-//            $timeOfCreation = $listingDetails["Time_Of_Creation"];
-//            $postCode = $listingDetails["Post_Code"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//            // Owner's details
-//            $offeringUserID = $listingDetails["UserID"];
-//            $offeringUserName = $listingDetails["Forename"] . " " . $listingDetails["Surname"];
-//            $offeringUserImage = $this->cardDetailsModel->getUserImage($offeringUserID);
-//            $hasRated = $this->model->hasRated($transactionID, $this->getUserID());
-//            $item = array(
-//                "transactionID" => $transactionID,
-//                "completedDate" => $completedDate,
-//                "userImg" => $offeringUserImage,
-//                "userID" => $offeringUserID,
-//                "userName" => $offeringUserName,
-//                "addedDate" => $timeOfCreation,
-//                "distance" => "CAN'T GET THIS INFORMATION",
-//                "imgURL" => $imageURL,
-//                "itemName" => $itemName,
-//                "quantity" => $transactionQuantity,
-//                "listingID" => $listingID,
-//                "postCode" => $postCode,
-//                "hasRated" => $hasRated
-//            );
-//            array_push($completedRequests, $item);
-//        }
-//        // Pending requesting transactions
-//        foreach ($pendingReceiving as $transactionID) {
-//            $transactionDetails = $this->model->getDetailsFromTransactionID($transactionID);
-//            // Info specific to transaction
-//            $transactionQuantity = $transactionDetails["Quantity"];
-//            $startedDate = $transactionDetails["Time_Of_Application"];
-//            // Information about the item/listing involved
-//            $listingID = $transactionDetails["ListingID"];
-//            $listingDetails = $this->cardDetailsModel->getCardDetails($listingID);
-//            $itemName = $listingDetails["Name"];
-//            $timeOfCreation = $listingDetails["Time_Of_Creation"];
-//            $postCode = $listingDetails["Post_Code"];
-//            $defaultImage = $this->cardDetailsModel->getDefaultImage($listingID);
-//            $imageURL = $defaultImage["Image_URL"];
-//            // Owner's details
-//            $offeringUserID = $listingDetails["UserID"];
-//            $offeringUserName = $listingDetails["Forename"] . " " . $listingDetails["Surname"];
-//            $offeringUserImage = $this->cardDetailsModel->getUserImage($offeringUserID);
-//            $item = array(
-//                "transactionID" => $transactionID,
-//                "startedDate" => $startedDate,
-//                "userImg" => $offeringUserImage,
-//                "userID" => $offeringUserID,
-//                "userName" => $offeringUserName,
-//                "addedDate" => $timeOfCreation,
-//                "distance" => "CAN'T GET THIS INFORMATION",
-//                "imgURL" => $imageURL,
-//                "itemName" => $itemName,
-//                "quantity" => $transactionQuantity,
-//                "listingID" => $listingID,
-//                "conversationID" => "SAME AS LISTING ID?",
-//                "postCode" => $postCode
-//            );
-//            array_push($pendingRequests, $item);
-//        }
-//        $userListings = array("available" => $allAvailableListings, "outOfStock" => $allEmptyListings);
-//        $offers = array("completed" => $completedOffers, "pending" => $pendingOffers);
-//        $requests = array("completed" => $completedRequests, "pending" => $pendingRequests);
-//
-//        $listingsInformation = array(
-//            "listingsCount" => $totalAvailabaleListings, // Total number of listings with quantity > 0
-//            "emptyListingsCount" => $totalEmptyListings, // Total number of listings with quantity <= 0
-//            "itemsOfferedCount" => $sendingTransactionsCount, // Total of all transactions for your items (can be greater than listings count)
-//            "requestsMadeCount" => $receivingCount, // Total of all transactions you're in for other user's items
-//            "userListings" => $userListings, // All your listings
-//            "offers" => $offers, // Transactions for your items
-//            "requests" => $requests // Transactions for other user's items
-//        );
-//        return $listingsInformation;
-//    }
 
     /* Generates HTML for Home tab */
     function generateHomeSection()
@@ -849,5 +576,15 @@ class ProfilePageController
             $this->model->addToWatchList($listingID, $this->getUserID());
             return 2; // Code for added to watch list
         }
+    }
+    
+    public setAllPendingAsViewed(){
+        $listingTransactions = $this->model->getUnseenPendingTransactions();
+        foreach($listingTransactions as $listingTransaction){
+            $transactionID = $listingTransaction["TransactionID"];
+            $listingID = $listingTransaction["ListingID"];
+            $this->requestModel->setViewed($listingID, $transactionID);
+        }
+        return True;
     }
 }
