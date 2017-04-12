1e6ede388a57c005d809933d472ae37451940d8b
stephenc29 <stephen.church@durham.ac.uk>
2017-04-12 15:56:54 +0100

    Create notifications.js

---
+++ public/js/notifications.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  function updateNotifications(){    
+      var url = window.location.protocol + "//" + window.location.host + "/" + 'notifications/update';
+      $.getJSON(url, function(response){
+          $requestNotifications = response["requestNotifications"];
+          $messageNotifications = response["messageNotifications"];
+          
+          // Update relevant parts of the page
+      });
+  });
+});
