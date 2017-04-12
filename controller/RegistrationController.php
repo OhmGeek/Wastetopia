1dd29aca5fd52d78d9e0b5a7aaf095ab8339d859
stephenc29 <stephen.church@durham.ac.uk>
2017-04-12 14:00:21 +0100

    Update RegistrationController.php

--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -166,6 +166,7 @@ class RegistrationController
         $mail->Host = "mail3.gridhost.co.uk"; // For SSL, use mail3.gridhost.co.uk, else try mail.ohmgeek.co.uk
         $mail->Port = 465; //25 for non-SSL, 465  for SSL
         $mail->SMTPSecure = 'ssl'; 
+        $mail->SMTPDebug = 2;
         $mail->SMTPAuth = true;     // turn off SMTP authentiocation
         $mail->Username = "wastetopia@ohmgeek.co.uk";  // SMTP username
         $mail->Password = "wyI4wwPRhHGk"; // SMTP password (IHatePHP  or wyI4wwPRhHGk)
