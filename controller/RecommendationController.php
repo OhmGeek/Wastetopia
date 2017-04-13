81f380208151d061c84004bc4aaf3636d5a18132
stephenc29 <stephen.church@durham.ac.uk>
2017-04-13 09:56:51 +0100

    Update RecommendationController.php

--- controller/RecommendationController.php
+++ controller/RecommendationController.php
@@ -67,6 +67,7 @@ class RecommendationController {
           $isRequesting = $this->profilePageModel->isRequesting($listingID, $this->getUserID());
           $isWatching = $this->profilePageModel->isWatching($listingID, $this->getUserID());
           $item = array(
+            "section" => "recommmendation"  
             "listingID" => $listingID,
             "userImg" => $userImage,
             "userID" => $userID,
@@ -96,6 +97,73 @@ class RecommendationController {
         return $template->render($output); // Render with cardDetails for listings that match most frequent tags
       }
     
-    
+       
+      /**
+    * Generates the HTML for the cards in a prediction of similar items you may give away section
+    */
+    function generatePredictionSection(){
+      $frequentTags = $this->model->getTagFrequenciesForListings();
+      
+      // Deal with if there are not enough tags
+      
+      // Extract 5 most frequent tags
+      $tags = array();
+      
+      for($x = 0; $x < 5; $x++){
+          $tagDetails = $frequentTags[x];
+          $tagID = $tagDetails["TagID"];
+          array_push($tags, $tagID);
+      }
+      
+      // Use search query using $tags to find listings that match these tags
+      // Get user's lat/long 
+      // get $results
+      $searchController = new SearchController();
+      $results = $searchController->recommendationSearch($tags);
+        
+      $recommendationList = array();  
+      foreach($results as $listing){
+          $listingID = $listing["ListingID"];
+          $userID = $listing["UserID"];
+          $userImage = $this->cardDetailsModel->getUserImage($userID);//$listing[""]; // Needs adding
+          $userName = $listing["Forename"]." ".$listing["Surname"];
+          $addedDate = $listing["Time_Of_Creation"];
+          $distance = "DON'T HAVE"; // May be able to add later
+          $postCode = $listing["Post_Code"];
+          $imgURL = $this->cardDetailsModel->getDefaultImage($listingID);//$listing[""]; // Add later
+          $itemName = $listing["Name"];
+          $quantity = $listing["Quantity"];
+          $isRequesting = $this->profilePageModel->isRequesting($listingID, $this->getUserID());
+          $isWatching = $this->profilePageModel->isWatching($listingID, $this->getUserID());
+          $item = array(
+            "section" => "prediction",  
+            "listingID" => $listingID,
+            "userImg" => $userImage,
+            "userID" => $userID,
+            "userName" => $userName,
+            "addedDate" => $addedDate,
+            "postCode" => $postCode,
+            "imgURL" => $imgURL,
+            "itemName" => $itemName,
+            "quantity" => $quantity,
+            "isRequesting" => $isRequesting,
+            "isWatching" => $isWatching  
+          );
+          
+          array_push($recommendationList, $item);
+      }
+        
+      $currentConfig = new CurrentConfig();
+      $config = $currentConfig->getAll();  
+        
+      $output = array(
+            "config" => $config,
+            "recommendationList" => $recommendationList
+      );
+        
+        $template = $this->twig->loadTemplate('/items/recommendations.twig');
+      
+        return $template->render($output); // Render with cardDetails for listings that match most frequent tags
+      }
 }
 
