315cf0551280ace350669cdef54fd2275197df3a
stephenc29 <stephen.church@durham.ac.uk>
2017-04-13 13:56:25 +0100

    Update RecommendationController.php

--- controller/RecommendationController.php
+++ controller/RecommendationController.php
@@ -120,8 +120,8 @@ class RecommendationController {
               $tagID = $tagDetails["TagID"];
               array_push($tags, $tagID);
           }
-          print_r("PREDICTIONS");
-            print_r($tags);
+//           print_r("PREDICTIONS");
+//             print_r($tags);
           // Use search query using $tags to find listings that match these tags
           // Get user's lat/long 
           // get $results
