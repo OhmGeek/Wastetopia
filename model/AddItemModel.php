325b886429bb6929acfe40ef0eddda783c979794
Ryan Collins <ryan@ohmgeek.co.uk>
2017-04-13 17:20:11 +0100

    Oops, forgot to remove bind

--- model/AddItemModel.php
+++ model/AddItemModel.php
@@ -57,7 +57,6 @@ class AddItemModel
          ");
 
 	$statement->bindValue(":name", $name, PDO::PARAM_STR);
-	$statement->bindValue(":useBy", $useBy, PDO::PARAM_STR);
 	$statement->bindValue(":description", $description, PDO::PARAM_STR);
     $statement->execute();
     $results = $statement->fetchAll(PDO::FETCH_ASSOC)[0];
