24f8ac84a6ca829016ad487325ddd1b684842185
Ryan Collins <ryan@ohmgeek.co.uk>
2017-04-14 20:43:21 +0100

    Adjusted model code

--- public/index.php
+++ public/index.php
@@ -200,16 +200,24 @@ $klein->with('/api', function () use ($klein) {
 });
 
 $klein->respond('POST', '/api/barcode/get', function($request, $response) {
+    // first of all, get the file we just posted.
+    $file = $request->files()[0];
+    $data = $file['tmp_name'];
+    // now let's define the things we need to send the request:
+    $headers = array("Content-Type:multipart/form-data"); // cURL headers for file uploading
+    $postfields = array("filedata" => "@$data", "filename" => $file['name']);
     $curl = curl_init();
     $timeout = 100;
     $ret = "";
     $url="https://zxing.org/w/decode";
     curl_setopt ($curl, CURLOPT_URL, $url);
     curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt ($curl, CURLOPT_HEADER, true);
+    curl_setopt ($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt ($curl, CURLOPT_MAXREDIRS, 20);
     curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt ($curl, CURLOPT_POSTFIELDS, "f=".$request->f);
+    curl_setopt ($curl, CURLOPT_POSTFIELDS, $postfields);
     curl_setopt ($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.5) Gecko/2008120122 Firefox/3.0.5");
     curl_setopt ($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
     $text = curl_exec($curl);
