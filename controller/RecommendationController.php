2ba0f5938e753820c0ece2ace8abcac44b110d65
Stephen Church <stephen.church@durham.ac.uk>
2017-04-14 18:11:02 +0100

    Testing message

--- controller/RecommendationController.php
+++ controller/RecommendationController.php
@@ -34,7 +34,9 @@ class RecommendationController {
     */
     function generateRecommendedSection(){
       $frequentTags = $this->model->getTagFrequenciesForTransactions();
-        
+
+      print_r($frequentTags);
+      
       // Deal with if there are not enough tags    
       if(count($frequentTags) < 3){
           $recommendationList = array(); // Empty array
