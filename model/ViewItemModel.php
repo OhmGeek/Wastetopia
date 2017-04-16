37ba59f06f5e90eb325bbabf16e54c7c6497e77e
Ryan Collins <ryan@ohmgeek.co.uk>
2017-04-16 13:07:52 +0100

    Use Array_Merge properly in PHP

--- model/ViewItemModel.php
+++ model/ViewItemModel.php
@@ -161,10 +161,10 @@ class ViewItemModel
      */
     function getAll($listingID){
         $itemSerialised = array();
-        array_push($itemSerialised, $this->getTagDetails($listingID));
-        array_push($itemSerialised, $this->getDetails($listingID));
-        array_push($itemSerialised, $this->getItemStatus($listingID));
-        array_push($itemSerialised, $this->getImages($listingID));
+        $itemSerialised = array_merge($itemSerialised, $this->getTagDetails($listingID));
+        $itemSerialised = array_merge($itemSerialised, $this->getDetails($listingID));
+        $itemSerialised = array_merge($itemSerialised, $this->getItemStatus($listingID));
+        $itemSerialised = array_merge($itemSerialised, $this->getImages($listingID));
         error_log(json_encode($itemSerialised));
         return (object) $itemSerialised;
     }
