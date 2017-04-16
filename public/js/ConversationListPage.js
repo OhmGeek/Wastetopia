1d6086fb39a29f27cfb3a9dffbc131330bb9e93a
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-16 20:38:28 +0100

    changed bottom padding for conversation part

--- public/js/ConversationListPage.js
+++ public/js/ConversationListPage.js
@@ -28,6 +28,7 @@ $(document).on('click', '#deleteBtn', function(){
   var url = window.location.protocol + "//" + window.location.host + "/" + 'messages/delete-conversation';
 
   var listingID = $(this).closest('thumbnail').attr('id');
+  console.log(listingID)
   var data = {listingID: listingID}
   $.get(url, data, function(response){
       loadUsers();
