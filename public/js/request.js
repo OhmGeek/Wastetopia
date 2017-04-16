72177702274816aca6ce4482f6d2bbb75a0a6fa3
stephenc29 <stephen.church@durham.ac.uk>
2017-04-16 10:57:48 +0100

    Routed buttons to messaging page

--- public/js/request.js
+++ public/js/request.js
@@ -629,15 +629,15 @@ $(function () {
     });
 
 
-    // // View conversation
-    // $('a[href="#message"]'.click(function(){
-    //     var listingID = $(this).attr("id");
-    //    // Send to /messages/conversation/[:listingID]
-    //
-    //     var url = baseURL + "/messages/conversation/"+listingID;
-    //
-    //     location.href = url;
-    // });
+    // View conversation
+    $('a[href="#message"]'.click(function(){
+        var listingID = $(this).attr("id");
+       // Send to /messages/conversation/[:listingID]
+    
+        var url = baseURL + "/messages/conversation/"+listingID;
+    
+        location.href = url;
+    });
 
 
     // MODALS USED
