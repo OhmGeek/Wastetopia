1dc84fb49e6bd75c59eccb8f372471febde24fa2
stephenc29 <stephen.church@durham.ac.uk>
2017-04-16 20:02:08 +0100

    Added notifications to navbar

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -6,10 +6,23 @@ $(function(){
       var url = window.location.protocol + "//" + window.location.host + "/" + 'notifications/update';
       $.getJSON(url, function(response){
           // Extract data
-          $requestNotifications = response["requestNotifications"];
-          $messageNotifications = response["messageNotifications"];
-          
-          // Update relevant parts of the page
+          var requestNotifications = response["requestNotifications"];
+          var messageNotifications = response["messageNotifications"];
+        
+           // Update relevant parts of the page
+        if (requestNotifications == 0){
+            $("#requestNotifications").addClass("hide"); 
+        }else{
+            $("#requestNotifications").removeClass("hide");
+            $("#requestNotifications").html(requestNotifications);          
+        }
+        
+        if (messageNotifications == 0){
+            $("#messageNotifications").addClass("hide"); 
+        }else{
+            $("#messageNotifications").removeClass("hide");
+            $("#messageNotifications").html(messageNotifications);          
+        }
       });
   }
 
