fa8970917c52b2757d73f70214601352396f5bf5
stephenc29 <stephen.church@durham.ac.uk>
2017-04-16 12:01:38 +0100

    Update ConversationListModel.php

--- model/ConversationListModel.php
+++ model/ConversationListModel.php
@@ -127,8 +127,6 @@ class ConversationListModel
 
 	    
         $currentUser = $this->getUserID();
-print_r("Current: ".$currentUser);
-	    print_r("Listing: ".$listingID);
 		$statement = $this->db->prepare("INSERT INTO Conversation (FK_User_ReceiverID, FK_Listing_ListingID)
 									VALUES (:userID, :listingID)");
 
