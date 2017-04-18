8d61c5841bcff4c525489c64eccda02cf7523a2b
Ryan Collins <ryan@ohmgeek.co.uk>
2017-04-18 14:44:25 +0100

    Use same view for both add/edit£

---
+++ public/js/edit-item.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ryan on 18/04/17.
+ */
+
+// This file just does all the autofill stuff, ready for the queries to kick in.
+// Rest of the functionality is dealt with in the add-item.js file.
