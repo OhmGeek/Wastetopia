5e2681471317f2ddb45f41e1d2a156464e5cbe1e
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-04-18 10:42:37 +0100

    just checking if deploy to heroku

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 $(function(){
 
+$('.main-search-form').submit(function(e){
+  e.preventDefault();
+  var search = $('#search').val().trim()
+  var postcode = $('#postocode').val().trim()
 
+})
 
 })
