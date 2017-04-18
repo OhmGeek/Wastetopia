d1b16dd38124ab0247a286aa4b7a6b31b3d00a6a
Ryan Collins <ryan@ohmgeek.co.uk>
2017-04-18 15:07:05 +0100

    Add basic JS to fill, not fully finished

--- public/js/edit-item.js
+++ public/js/edit-item.js
@@ -4,9 +4,56 @@
 
 // This file just does all the autofill stuff, ready for the queries to kick in.
 // Rest of the functionality is dealt with in the add-item.js file.
+var Fill = function(data) {
+  var fillName = function() {
+      $('name').val(data.name);
+  };
+  var fillDescription = function() {
+      data.description;
+  };
 
+  var fillDate = function() {
+      data.expires;
+  };
+
+  var fillImages = function() {
+    data.images (autofill these);
+  };
+
+  var fillLocation = function() {
+      data.location
+          .postcode
+          .name
+
+  };
+
+  var fillType = function() {
+
+  };
+
+  var fillState = function() {
+
+  };
+
+  var fillDietary = function() {
+
+  };
+
+  var fillContains = function() {
+
+  };
+
+  var fillOther = function() {
+
+  };
+};
 function fillInTheGaps() {
-    $.get('https://wastetopia-pr-17.herokuapp.com/api/items/view/')
+    var listingID = $('.grid-body').data('listingID');
+    $.getJSON('https://wastetopia-pr-17.herokuapp.com/api/items/view/', data)
+        .done(function(data) {
+            var filler = new Fill(data);
+            filler.fill
+        });
 }
 
 $(document).ready(function() {
