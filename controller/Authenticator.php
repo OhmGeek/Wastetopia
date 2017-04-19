84c8551c15a192f8d2f26659fd2cf46fa3a79a31
Stephen Church <stephen.church@durham.ac.uk>
2017-04-19 13:56:11 +0100

    Error logs for authentication

--- controller/Authenticator.php
+++ controller/Authenticator.php
@@ -15,17 +15,16 @@ class Authenticator {
         $auth_token = $cookie->get_auth_token();
         $user_id = $cookie->get_user_id();
         error_log("Quick, verify the login");
+        error_log("User's auth token: ".$auth_token);
+        error_log("User's ID: ".$user_id);
+        error_log("Send to token manager");
         //verify
         if(TokenManager::verify($auth_token,$user_id)) {
             error_log("Login verified");
-            error_log($auth_token);
-            error_log($user_id);
             return true;
         }
         else {
             error_log("Not verified");
-            error_log($auth_token);
-            error_log($user_id);
             return false;
         }
     }
--- model/Token.php
+++ model/Token.php
@@ -6,13 +6,23 @@ class Token {
     private $after_salt = "EndSalt11!!!1";
 
     public static function generate_token($user_id) {
+        error_log("Before salt: ".$before_salt);
+        error_log("After salt: ".$after_salt);
+        error_log("Date: ".date("Y-m-d"));
+        error_log("Remote address: ".$_SERVER['REMOTE_ADDR']);
+        error_log("Host name: ".gethostname());
+
         $token_data = $before_salt. date("Y-m-d") . $_SERVER['REMOTE_ADDR'] . gethostname() . $user_id . $after_salt;
         $token = hash("sha256", $token_data);
         return $token;
     }
 
     public static function verify_token($auth_token, $user_id) {
+        error_log("Auth token: ".$auth_token);
+        error_log("User ID: ".$user_id);
+        error_log("Generate expected token");
         $expected_token = self::generate_token($user_id);
+        error_log("Expected token: ".$expected_token);
 	    return ($expected_token == $auth_token);
     }
 }
