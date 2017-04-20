1f00ecb1008477c3a9106957e59487d4c8441412
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2017-04-20 23:35:26 +0100

    fix form submit on index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,53 @@
 $(function(){
-  //fancy select
-  $(".js-example-basic-multiple").select2();
+    //fancy select
+    $(".js-example-basic-multiple").select2();
 
-  $('.main-search-form').submit(function(e){
-    e.preventDefault();
-    var search = $('#search').val().trim()
-    var postcode = $('#postcode').val().trim()
-    var quantity = $('#quantity').val().trim()
-    var distance = $('#distance').val().trim()
-    var sortBy = $('#sort').val()
-    var selectedFilters = []
+    $('.main-search-form').submit(function(e){
 
-    $('.main-search-form *').filter('.selects').each(function(){
-      var formGroup = $(this)
-      formGroup.find('select option:selected').each(function(){
-        var input = $(this)
-        selectedFilters.push(input.attr('id'))
-      });
+
+        var search = $('#search').val().trim()
+        var postcode = $('#postcode').val().trim()
+        var quantity = $('#quantity').val().trim()
+        var distance = $('#distance').val().trim()
+        var sortBy = $('#sort').val()
+        var selectedFilters = []
+
+        $('.main-search-form *').filter('.selects').each(function(){
+          var formGroup = $(this)
+          formGroup.find('select option:selected').each(function(){
+            var input = $(this)
+            selectedFilters.push(input.attr('id'))
+          });
+        });
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode( {'address':postcode, 'region':'GB'}, function(results, status){
+            if(status === google.maps.GeocoderStatus.OK){
+                var lat = results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng();
+                var baseURL = $('#baseURL').attr('href');
+                var searchURL = baseURL + '/search/' + search + '/' + postcode + '/' + lat + '/' + lng;
+
+                $('.main-search-form').attr("action", searchURL)
+
+                var newForm = '<form class="submit-form" action="'+ baseURL +'/search/'+ search +'/'+ postcode +'/'+ lat +'/'+ lng +'"> ' +
+                              '<input id="postcode" type="text" value="'+ postcode +'"></input> <input id="quantity" type="number" value="'+ quantity +'"></input>' +
+                              '<input id="distance" type="number" value="'+ distance +'"></input> <input id="filters" type="text" value="'+ selectedFilters.join('+') +'"></input>' +
+                              '<input id="sort" type="text" value="'+sortBy+'"></input> </form>"'
+
+
+                $('.secondary-search-form').html(newForm)
+                $('.submit-form').submit();
+
+            }
+            else if (status === google.maps.GeocoderStatus.ZERO_RESULTS){
+                window.alert("Could not find postcode");
+            }
+            else{
+            window.alert("There was an error processing your request. \n Please try again later");
+            }
+        });
+        return false;
     });
-
-    console.log(selectedFilters.join('+'))
-
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({
-      componentRestrictions: {
-        country: 'GB',
-        postalCode: postcode
-      }
-    },
-    function(results, status) {
-      if(status === google.maps.GeocoderStatus.OK){
-        var lat = results[0].geometry.location.lat();
-        var lng = results[0].geometry.location.lng();
-        var baseURL = $('#baseURL').attr('href');
-        var searchURL = baseURL + '/search/' + search + '/' + postcode + '/' + lat + '/' + lng;
-        $.post(searchURL, { filter : selectedFilters.join('+'), sort : sortBy, distance: distance, quantity: quantity}, function(event, status){
-          if (status === "success") {
-            location.href = searchURL
-          }else {
-            window.alert("Request could not be completed. Try again later.")
-          }
-        })
-      }
-      else if (status === google.maps.GeocoderStatus.ZERO_RESULTS){
-        window.alert("Could not find postcode");
-      }
-      else{
-        window.alert("There was an error processing your request. \n Please try again later");
-      }
-    });
-
-  })
-
-})
+});
