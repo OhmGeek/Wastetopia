d002d66174bebbb96183b7c3c205b800e3a8ef1b
stephenc29 <stephen.church@durham.ac.uk>
2017-04-21 16:20:06 +0100

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -182,7 +182,7 @@ $(function () {
 
 
     // Set all pending transactions to viewed
-    $(document).on('shown.bs.tab', 'a[href="#pending-transaction"]', function () {
+    $(document).on('shown.bs.tab', '#pending-transaction', function () {
         console.log("Setting pending as viewed");
         var url = window.location.protocol + "//" + window.location.host + "/profile/set-pending-viewed";
         console.log(url);
