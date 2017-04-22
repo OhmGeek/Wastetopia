f75f5f673cfaa9c78714830539a3a88b375f3576
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2017-04-22 16:18:18 +0100

    config

--- config/ProductionConfig.php
+++ config/ProductionConfig.php
@@ -26,11 +26,11 @@ class ProductionConfig extends AbstractConfig
             'COOKIE_IDENTIFIER' => 'gpwastetopiadata',
 
 
-            'ROOT_JS' => '//wastetopia-pr-35.herokuapp.com/js',
-            'ROOT_CSS' => '//wastetopia-pr-35.herokuapp.com/css',
+            'ROOT_JS' => '//wastetopia.herokuapp.com/js',
+            'ROOT_CSS' => '//wastetopia.herokuapp.com/css',
 
-            'ROOT_IMG' => '//wastetopia-pr-35.herokuapp.com/img',
-            'ROOT_BASE' => '//wastetopia-pr-35.herokuapp.com',
+            'ROOT_IMG' => '//wastetopia.herokuapp.com/img',
+            'ROOT_BASE' => '//wastetopia.herokuapp.com',
 
             'EMAIL_HOST' => 'smtp-mail.outlook.com',
             'EMAIL_ADDRESS' => 'wastetopia@outlook.com',
