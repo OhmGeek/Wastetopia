9c11e3f62d36b055a41f9c64c945b61a94be4abb
Ryan Collins <OhmGeek@users.noreply.github.com>
2017-04-24 10:22:52 +0100

    Remove dodgy code

--- public/js/edit-item.js
+++ public/js/edit-item.js
@@ -10,27 +10,6 @@ var mode = $('#main-container').data("mode");
 console.log(listingID);
 console.log(mode);
 
-showUploadedItem = function(url, id) {
-      var $item = $('<div class="grid-item col-xs-6 col-md-4 zero-padding">'+
-                    '<div class="img-checkbox">'+
-                      '<div class="checkbox">'+
-                        '<label><input type="checkbox"></label>'+
-                      '</div>'+
-                    '</div>'+
-                    '<div data-mh="my-group" class="upload-pic">'+
-                      '<img src="'+ url +'" data-imgid="' + id + '"/>'+
-                    '</div>'+
-                  '</div>');
-
-        // prepend items to grid
-        $grid.prepend( $item )
-        // add and lay out newly prepended items
-            .isotope( 'prepended', $item );
-        // var currentImgs = $item + $('#img-rows').html();
-        // $('#img-rows').html(currentImgs)
-        $.material.init();
-    }
-
 // only autofill images if we can edit items
 if(mode == "edit") {
     $.getJSON($('base').attr('href') + '/api/items/view/' + listingID)
